c1a6ef270b88f12f456a0aa0f2982fc46db67015
ozguraltug <111081045+ozguraltug@users.noreply.github.com>
2023-09-06 23:28:58 +0300

    Reacted:Advance SQL

--- 13 - AdvancedSQL/AdvanceSQL.sql
+++ 13 - AdvancedSQL/AdvanceSQL.sql
@@ -232,10 +232,360 @@ begin
 	into count_rows;
 	
 	if count_rows > 0 then
-	insert into film (title)
+	insert into film (id,title)
 	values (5,'Yeşil Yol');
 	raise notice 'Yeni film eklendi.';
 	else raise notice 'Film tablosu boş';
 	end if;
 end $$;
+--*******************************************************
+-- ************* IF-THEN-ELSE-IF ************************
+--*******************************************************
+
+--syntax:
+if conditon_1 then
+	statemnet_1;
+	elsif condition_2 then
+		statement_2 then
+	elseif condition_3 then
+		statement_3;....
+	else 
+		else_statemnet;
+end if;
+
+--Task : 1 id li film varsa ; 
+			suresi 50 dakikanin altinda ise Kısa, 
+			50<length<120 ise Orta, 
+			length>=120 ise Uzun yazalim
+			
+do $$
+declare
+	v_film film%rowtype;
+	len_description varchar(50);
+begin
+	select * from film
+	into v_film
+	where id=1;
+	
+	if not found then
+	raise notice 'Film bulunamadı';
+	else
+		if v_film.length > 0 and v_film.length<50 then
+			len_description='Kısa';
+		elsif v_film.length>50 and v_film.length<120 then
+			len_description='Orta';
+		elseif v_film.length>=120 then
+			len_description='Uzun';
+		else
+			len_description='Tanımlanamıyor';
+		end if;
+	raise notice '% filmin süresi : %', v_film.title,len_description;
+	end if;
+
+end $$;
+--***************************************************
+-- ******** Case Statement **************************
+--***************************************************
+--syntax:
+case search-expression 
+	when expression then
+	statement
+	[....
+	else
+		else-statement]
+		
+end case;
+-- Task : Filmin turune gore cocuklara uygun olup olmadigini ekrana yazalim
+
+do $$
+declare
+	tur film.type%type;
+	uyari varchar(50);
+begin
+	select type
+	from film
+	into tur
+	where id=1;
+	
+	if found then
+	case tur
+		when 'Korku' then
+			uyari='Çocuklar için uygun değil.';
+		when 'Macera' then
+			uyari='Ebeveyn tercihinde izleyebilir';
+		when 'Animasyon' then
+			uyari='Tavsiye edilir';
+		else 
+			uyari='Tanımlanmayan tür.';
+		end case;
+		raise notice '% türü %',tur,uyari;
+	end if;
+end $$;
+--******************************************
+--************** LOOP **********************
+--******************************************
+--syntax--1.örnek
+<<label>>
+loop
+	statement;
+end loop;
+
+--loop u sonlandırmak için if yapısını kullanabiliriz
+<<label>>--2.örnek
+loop
+	statement;
+	if condition then
+		exit;
+	end if;
+end loop;
+
+--nested loop örneği-3.örnek
+<<outer>>
+loop
+	statement;
+	<<inner>>
+	loop
+	/*...*/
+	exit;--inner loop tan çıkış
+	end loop;
+	exit;--outer loop tan çık
+	end loopİ outer loop u bitir
+
+--Task :loop kullanarak ekrana counter 1 den 4 e kadar yazdırın
+do $$
+declare
+	counter integer :=0;
+begin
+	loop
+	exit when counter=4;
+	counter=counter+1;
+	raise notice 'counter %',counter;
+	end loop;
+
+end $$;
+
+-- Task : Fibonacci serisinde, belli bir siradaki sayiyi ekrana getirelim
+--Fibonacci sayıları 1, 1, 2, 3, 5, 8, 12, 21, 34, 55...
+do $$
+declare
+	n integer:=10;
+	counter integer:=0;
+	i integer:=0;
+	j integer:=1;
+	fib integer:=0;
+	
+begin
+	if n<1 then
+		fib:=0;
+	end if;
+	loop 
+	exit when counter=n;
+	counter=counter+1;
+	select j,i+j into i,j;  -- seçim(0,1)->(1,1)/2.işlem de seçim(1,1)->(1,2)/3.işlem(1,2)->(2,3)
+	end loop;
+	fib:=i;
+	raise notice '%',fib;
+		
+end $$;
+
+--Task  : Film tablosundaki film sayisi 10 dan az ise "Film sayisi az" yazdirin, 
+--10 dan cok ise "Film sayisi yeterli" yazdiralim
+do $$
+declare
+	sayi integer:=0;
+begin
+	select count(*)
+	from film
+	into sayi;
+	
+	if sayi<10 then
+		raise notice 'Film sayısı az';
+		else
+		raise notice 'Film sayısı yeterli';
+	end if;
+end $$;
+
+--Ödev
+-- Task : user_age isminde integer data turunde bir degisken tanimlayip default olarak bir deger verelim, 
+--If yapisi ile girilen deger 18 den buyuk ise Access Granted, kucuk ise Access Denied yazdiralim 
+
+-- Task : a ve b isimli integer turunde 2 degisken tanimlayip default degerlerini verelim, 
+--eger a nin degeri b den buyukse "a , b den buyuktur" yazalim, tam tersi durum icin "b, a dan buyuktur" yazalim, 
+--iki deger birbirine esit ise " a,  b'ye esittir" yazalim: 
+
+-- Task : kullaniciYasi isimli bir degisken olusturup default degerini verin, girilen yas 18 den buyukse "Oy kullanabilirsiniz", 
+--18 den kucuk ise "Oy kullanamazsiniz" yazisini yazalim.
+
+--**************************************************
+-- ************ WHILE LOOP *************************
+--**************************************************
+--syntax
+while condition loop
+	statement;
+end loop;
+-- Task : 1 dan 4 e kadar counter degerlerini ekrana basalim
+
+do $$
+declare
+	n integer:=4;
+	counter integer:=0;
+begin
+	while counter<n loop
+		counter=counter+1;
+	raise notice '%',counter;
+	end loop;
+
+end $$;
+--***********************************************
+-- **************  FOR LOOP *********************
+--***********************************************
+--syntax
+FOR loop_counter IN [REVERSE] FROM..TO [ by step] LOOP
+	statement;
+	END LOOP;
+
+--Örnek 1 in kullnımı
+do $$
+
+begin
+	for counter in 1..5 loop 
+		raise notice 'counter : %',counter;
+	end loop;
+end $$;
+--Not:değişken kullanılmayacakse declare yazılmasına gerek yok
+
+--Örnek 2 reverse kullanımı
+do $$
+begin
+	for counter in reverse 5..1 loop
+		raise notice 'counter : %',counter;
+	end loop;
+	
+end $$
+
+--3.Örnek by kullanımı
+do $$
+
+begin
+	for counter in 1..10 by 2 loop
+		raise notice 'counter : %',counter;
+	end loop;
+end $$;
+
+-- Task : 10 dan 20 ye kadar 2 ser 2 ser ekrana sayilari basalim : 
+
+do $$
+begin
+	for counter in 10..20 by 2 loop
+		raise notice 'conter : %',counter;
+	end loop;
+end $$;
+
+--dizilerin elemanları içinde gezinme
+do $$
+declare
+	array_int int[]=array[11,22,33,44,55,66,77,88];
+	var int[];
+begin
+	for var in select array_int loop
+	raise notice '%',var;
+	end loop;
+
+end $$;
+
+
+--db de loop örneği
+--syntax
+FOR target IN query loop
+	statement;
+end loop;
+-- Task : Filmleri suresine gore siraladigimizda en uzun 2 filmi gosterelim
+do $$
+declare
+ f record;
+
+begin
+	for f in select title,length
+	from film
+	order by length desc
+	limit 2
+	
+	loop
+		raise notice '% (% dakika )',f.title,f.length;
+	end loop;
+end $$;
+
+CREATE TABLE employees (
+  employee_id serial PRIMARY KEY,
+  full_name VARCHAR NOT NULL,
+  manager_id INT
+);
+
+INSERT INTO employees (
+  employee_id,
+  full_name,
+  manager_id
+)
+VALUES
+  (1, 'M.S Dhoni', NULL),
+  (2, 'Sachin Tendulkar', 1),
+  (3, 'R. Sharma', 1),
+  (4, 'S. Raina', 1),
+  (5, 'B. Kumar', 1),
+  (6, 'Y. Singh', 2),
+  (7, 'Virender Sehwag ', 2),
+  (8, 'Ajinkya Rahane', 2),
+  (9, 'Shikhar Dhawan', 2),
+  (10, 'Mohammed Shami', 3),
+  (11, 'Shreyas Iyer', 3),
+  (12, 'Mayank Agarwal', 3),
+  (13, 'K. L. Rahul', 3),
+  (14, 'Hardik Pandya', 4),
+  (15, 'Dinesh Karthik', 4),
+  (16, 'Jasprit Bumrah', 7),
+  (17, 'Kuldeep Yadav', 7),
+  (18, 'Yuzvendra Chahal', 8),
+  (19, 'Rishabh Pant', 8),
+  (20, 'Sanju Samson', 8);
+  
+    -- Task :  manager ID si en buyuk ilk 10 kisiyi ekrana yazalim 
+
+do $$
+declare
+	f record;
+
+begin
+	for f in select full_name,manager_id
+	from employees
+	order by manager_id desc
+	limit 10
+	
+	loop
+	raise notice '% - %',f.manager_id,f.full_name;
+	end loop;
+
+end $$;
+
+--*****************************************
+-- ************ CONTINUE ******************
+--*****************************************
+
+--syntax
+continue when condition ...
+-- Task : continue yapisi kullanarak 1 dahil 10 a kadar olan tek sayilari ekrana basalim
+do $$
+declare
+	counter integer:=0;
+
+begin
+	loop
+		counter=counter+1;--loop içinde counter değeri 1 artar
+		exit when counter>10;--counter 10 olunca lop tan çıkar
+		continue when mod(counter,2)=0; --counter sayısının 2 ile bölümünden kalan o ise atlar
+		raise notice '%',counter;
+	end loop;
+end $$;
+
+
+
 
