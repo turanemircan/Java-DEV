a29182f901a2bfb4fd1cd4f6b15636dbd297b4a8
Mirac <mirac.techproed@gmail.com>
2023-09-13 23:16:58 +0300

    contactMessage CRUD operations

--- 15 - BackendProject/student-management/src/main/java/com/project/contactmessage/service/ContactMessageService.java
+++ 15 - BackendProject/student-management/src/main/java/com/project/contactmessage/service/ContactMessageService.java
@@ -2,8 +2,11 @@ package com.project.contactmessage.service;
 
 import com.project.contactmessage.dto.ContactMessageRequest;
 import com.project.contactmessage.dto.ContactMessageResponse;
+import com.project.contactmessage.dto.ContactMessageUpdateRequest;
 import com.project.contactmessage.entity.ContactMessage;
+import com.project.contactmessage.exception.ResourceNotFoundException;
 import com.project.contactmessage.mapper.ContactMessageMapper;
+import com.project.contactmessage.message.Messages;
 import com.project.contactmessage.repository.ContactMessageRepository;
 import com.project.payload.response.ResponseMessage;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Service
@@ -47,5 +51,71 @@ public class ContactMessageService {
 
         return contactMessageRepository.findAll(pageable).map(contactMessageMapper::contactMessageToResponse);
 
+    }
+
+    // Not: searchByEmail ***************************************
+
+    public Page<ContactMessageResponse> searchByEmail(String email, int page, int size, String sort, String type) {
+        Pageable pageable = PageRequest.of(page,size, Sort.by(sort).ascending());
+        if(Objects.equals(type,"desc")){
+            pageable = PageRequest.of(page,size, Sort.by(sort).descending());
+        }
+
+        return contactMessageRepository.findByEmailEquals(email,pageable).map(contactMessageMapper::contactMessageToResponse);
+
+    }
+
+    // Not: searchBySubject *************************************
+    public Page<ContactMessageResponse> searchBySubject(String subject, int page, int size, String sort, String type) {
+        Pageable pageable = PageRequest.of(page,size, Sort.by(sort).ascending());
+        if(Objects.equals(type,"desc")){
+            pageable = PageRequest.of(page,size, Sort.by(sort).descending());
+        }
+
+        return contactMessageRepository.findBySubjectEquals(subject,pageable).map(contactMessageMapper::contactMessageToResponse);
+    }
+
+    // Not: getById *********************************************
+    public ContactMessage getContactMessageById(Long contactMessageId) {
+
+       return contactMessageRepository.findById(contactMessageId).orElseThrow(()->
+               new ResourceNotFoundException(Messages.NOT_FOUND_MESSAGE));
+
+    }
+
+    // Not: deleteByIdParam *************************************
+    public String deleteById(Long contactMessageId) {
+
+
+       // contactMessageRepository.delete(getContactMessageById(contactMessageId));
+        getContactMessageById(contactMessageId);
+        contactMessageRepository.deleteById(contactMessageId);
+        return Messages.CONTACT_MESSAGE_DELETE;
+    }
+    // Not: updateById ******************************************
+    public ResponseMessage<ContactMessageResponse> updateById(Long id, ContactMessageUpdateRequest contactMessageUpdateRequest) {
+        ContactMessage contactMessage = getContactMessageById(id);
+
+        if(contactMessageUpdateRequest.getMessage() !=null){
+            contactMessage.setMessage(contactMessageUpdateRequest.getMessage());
+        }
+        if(contactMessageUpdateRequest.getSubject() != null) {
+            contactMessage.setSubject(contactMessageUpdateRequest.getSubject());
+        }
+        if(contactMessageUpdateRequest.getName() != null){
+            contactMessage.setName(contactMessageUpdateRequest.getName());
+        }
+        if(contactMessageUpdateRequest.getEmail() != null) {
+            contactMessage.setEmail(contactMessageUpdateRequest.getEmail());
+        }
+
+        contactMessage.setDateTime(LocalDateTime.now());
+        contactMessageRepository.save(contactMessage);
+
+        return ResponseMessage.<ContactMessageResponse>builder()
+                .message("Contact Message Updated Successfully")
+                .httpStatus(HttpStatus.CREATED)
+                .object(contactMessageMapper.contactMessageToResponse(contactMessage))
+                .build();
     }
 }
