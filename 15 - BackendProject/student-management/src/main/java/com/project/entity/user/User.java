da917069ecd64f48eff8b643b2f017c14b2c22e1
Mirac <mirac.techproed@gmail.com>
2023-09-15 23:27:26 +0300

    entities

--- 15 - BackendProject/student-management/src/main/java/com/project/entity/user/User.java
+++ 15 - BackendProject/student-management/src/main/java/com/project/entity/user/User.java
@@ -1,11 +1,18 @@
 package com.project.entity.user;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.entity.business.LessonProgram;
+import com.project.entity.business.Meet;
+import com.project.entity.business.StudentInfo;
+import com.project.entity.enums.Gender;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -58,4 +65,32 @@ public class User {
     private Boolean isAdvisor;
 
     private Long advisorTeacherId; // bu field student lar icin eklendi
+
+    private Gender gender;
+
+    @OneToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private UserRole userRole;
+
+
+    @OneToMany(mappedBy = "teacher",cascade = CascadeType.REMOVE)
+    private List<StudentInfo> studentInfos; // set olabilir ??
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_lessonprogram",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "lesson_program_id")
+    )
+    private Set<LessonProgram> lessonsProgramList;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "meet_student_table",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "meet_id")
+    )
+    private List<Meet> meetList;
 }
