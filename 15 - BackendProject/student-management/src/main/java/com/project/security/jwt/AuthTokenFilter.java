e7a2ab9f5468ca7c336b4666338f81318ac7be0c
Mirac <mirac.techproed@gmail.com>
2023-09-18 23:10:17 +0300

    security- login

--- 15 - BackendProject/student-management/src/main/java/com/project/security/jwt/AuthTokenFilter.java
+++ 15 - BackendProject/student-management/src/main/java/com/project/security/jwt/AuthTokenFilter.java
@@ -4,6 +4,7 @@ import com.project.security.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,14 +21,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class AuthTokenFilter extends OncePerRequestFilter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthTokenFilter.class);
 
-    private final JwtUtils jwtUtils;
+    @Autowired
+    private  JwtUtils jwtUtils;
 
-    private final UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private  UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
