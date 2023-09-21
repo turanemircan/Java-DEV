eaea25e5070c764c177a60c898c5fd82786deb7c
Mirac <mirac.techproed@gmail.com>
2023-09-21 23:20:46 +0300

    userService- getAll-delete-update

--- 15 - BackendProject/student-management/src/main/java/com/project/payload/mappers/UserMapper.java
+++ 15 - BackendProject/student-management/src/main/java/com/project/payload/mappers/UserMapper.java
@@ -3,6 +3,9 @@ package com.project.payload.mappers;
 import com.project.entity.user.User;
 import com.project.payload.request.abstracts.BaseUserRequest;
 import com.project.payload.request.user.UserRequest;
+import com.project.payload.response.abstracts.BaseUserResponse;
+import com.project.payload.response.user.StudentResponse;
+import com.project.payload.response.user.TeacherResponse;
 import com.project.payload.response.user.UserResponse;
 import org.springframework.stereotype.Component;
 
@@ -44,4 +47,42 @@ public class UserMapper {
     }
 
 
+    public StudentResponse mapUserToStudentResponse(User student) {
+
+        return StudentResponse.builder()
+                .userId(student.getId())
+                .username(student.getUsername())
+                .name(student.getName())
+                .surname(student.getSurname())
+                .birthDay(student.getBirthDay())
+                .birthPlace(student.getBirthPlace())
+                .phoneNumber(student.getPhoneNumber())
+                .gender(student.getGender())
+                .email(student.getEmail())
+                .fatherName(student.getFatherName())
+                .motherName(student.getMotherName())
+                .ssn(student.getSsn())
+                .studentNumber(student.getStudentNumber())
+                .isActive(student.isActive())
+                .build();
+    }
+
+    public TeacherResponse mapUserToTeacherResponse(User teacher) {
+
+        return TeacherResponse.builder()
+                .userId(teacher.getId())
+                .username(teacher.getUsername())
+                .name(teacher.getName())
+                .surname(teacher.getSurname())
+                .birthDay(teacher.getBirthDay())
+                .birthPlace(teacher.getBirthPlace())
+                .ssn(teacher.getSsn())
+                .phoneNumber(teacher.getPhoneNumber())
+                .gender(teacher.getGender())
+                .email(teacher.getEmail())
+                .lessonPrograms(teacher.getLessonsProgramList())
+                .isAdvisorTeacher(teacher.getIsAdvisor())
+                .build();
+
+    }
 }
